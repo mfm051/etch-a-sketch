067aec92e4319374f30431dac1aca7ed7cbde43b
mfm051 <mateusmatos051@gmail.com>
2023-03-01 21:35:45 -0300

    Add eraser option

--- script.js
+++ script.js
@@ -34,6 +34,12 @@ function printSquares (numOfSquaresPerLine,canvas) {
 printSquares(numOfSquaresPerLine, canvas);
 
 // Pick a color
+let defaultColor = "#05BE05";
+let red = "#F33B1B";
+let yellow = "#EBFF0F";
+let blue = "#27EBF6";
+let canvasColor = "black";
+
 function changeCanvasStyle (color) {
     canvas.style.boxShadow = `0 0 50px ${color}`;
     titleText.style.color = color;
@@ -46,14 +52,9 @@ document.querySelector(".green").addEventListener("mousedown", () => {currentCol
 document.querySelector(".red").addEventListener("mousedown", () => {currentColor = red; changeCanvasStyle(red)});
 document.querySelector(".yellow").addEventListener("mousedown", () => {currentColor = yellow; changeCanvasStyle(yellow)});
 document.querySelector(".blue").addEventListener("mousedown", () => {currentColor = blue; changeCanvasStyle(blue)});
+document.querySelector(".eraser").addEventListener("mousedown", () => {currentColor = canvasColor; changeCanvasStyle(defaultColor)});
 
 // Change color of square
-let defaultColor = "#05BE05";
-let red = "#F33B1B";
-let yellow = "#EBFF0F";
-let blue = "#27EBF6";
-let canvasColor = "black";
-
 let currentColor = defaultColor;
 
 let titleText = document.querySelector(".text");
@@ -63,8 +64,13 @@ let squares = document.querySelectorAll(".square");
 function draw(e, color) {
     if (e.buttons === 1) {
         e.target.style.backgroundColor = color;
-        e.target.style.borderColor = color;
-        e.target.style.boxShadow = `0 0 10px ${color}`;
+        if (color === canvasColor) {
+            e.target.style.borderColor = "#393B39";
+            e.target.style.boxShadow = "none";
+        } else {
+            e.target.style.borderColor = color;
+            e.target.style.boxShadow = `0 0 10px ${color}`;
+        }
     } 
 };
 
