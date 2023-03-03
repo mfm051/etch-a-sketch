d692ebd929aefd1210d1c028993afd7e179dd4bc
mfm051 <mateusmatos051@gmail.com>
2023-03-02 21:03:41 -0300

    Update code to listen to changes in num of squares

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Create squares
 let numOfSquaresSlider = document.getElementById("num-of-squares");
-let numOfSquaresPerLine = 16;
 
 function getSquareLength (numOfSquaresPerLine) {
     let squareLength = (500/numOfSquaresPerLine - 2) //-2px of border
@@ -11,7 +10,6 @@ numOfSquaresSlider.addEventListener("input", (e) => numOfSquaresPerLine = e.targ
 
 // Print squares
 let canvas = document.querySelector(".canvas");
-let squares = document.querySelectorAll(".square");
 
 function printSquares (numOfSquaresPerLine,canvas) {
     let squareLength = getSquareLength(numOfSquaresPerLine);
@@ -24,13 +22,9 @@ function printSquares (numOfSquaresPerLine,canvas) {
     }
 }
 
-printSquares(numOfSquaresPerLine, canvas);
-
-numOfSquaresSlider.addEventListener("change", () => {   canvas.textContent = ""; 
-                                                        getSquareLength(numOfSquaresPerLine); 
-                                                        printSquares(numOfSquaresPerLine, canvas);
-                                                        squares = document.querySelectorAll(".square"); // # of squares changes
-})
+let numOfSquaresPerLine = numOfSquaresSlider.value; 
+printSquares(numOfSquaresPerLine, canvas);          
+let squares = document.querySelectorAll(".square"); 
 
 // Pick a color
 let defaultColor = "#05BE05";
@@ -38,6 +32,10 @@ let red = "#F33B1B";
 let yellow = "#EBFF0F";
 let blue = "#27EBF6";
 let canvasColor = "black";
+
+let currentColor = defaultColor;
+
+let titleText = document.querySelector(".text");
 
 function changeCanvasStyle (color) {
     canvas.style.boxShadow = `0 0 50px ${color}`;
@@ -54,35 +52,43 @@ document.querySelector(".blue").addEventListener("mousedown", () => {currentColo
 document.querySelector(".eraser").addEventListener("mousedown", () => {currentColor = canvasColor; changeCanvasStyle(defaultColor)});
 
 // Change color of square
-let currentColor = defaultColor;
-let titleText = document.querySelector(".text");
-
-
-function draw(e, color) {
+function draw(e) {
     if (e.buttons === 1) {
-        e.target.style.backgroundColor = color;
-        if (color === canvasColor) {
+        e.target.style.backgroundColor = currentColor;
+        if (currentColor === canvasColor) {
             e.target.style.borderColor = "#393B39";
             e.target.style.boxShadow = "none";
         } else {
-            e.target.style.borderColor = color;
-            e.target.style.boxShadow = `0 0 10px ${color}`;
+            e.target.style.borderColor = currentColor;
+            e.target.style.boxShadow = `0 0 10px ${currentColor}`;
         }
     } 
 };
 
-canvas.addEventListener("mousedown", (e) => {draw(e, currentColor)});
-squares.forEach((square) => square.addEventListener("mousemove",(e) => draw(e, currentColor)));
+function updateSquares (squares) {
+    squares.forEach((square) => {   square.addEventListener("mousemove", (e) => draw(e)); 
+                                    square.addEventListener("mousedown", (e) => draw(e))});
+};
+
+updateSquares(squares);
 
 // Reset canvas
 reset = document.getElementById("reset");
 
 function resetCanvas () {
+    squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
         square.style.backgroundColor = canvasColor;
         square.style.borderColor = "#393B39";
         square.style.boxShadow = "none"; 
-    })
+    });
 };
 
 reset.addEventListener("mousedown", resetCanvas);
+
+// Change number of squares
+numOfSquaresSlider.addEventListener("change", 
+                                    () => { canvas.textContent = "";
+                                            printSquares(numOfSquaresPerLine, canvas);
+                                            squares = document.querySelectorAll(".square");
+                                            updateSquares(squares)});
